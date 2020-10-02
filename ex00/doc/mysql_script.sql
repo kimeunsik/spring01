a8b76c373c23707153d245b65f88685494abcac2
kimeunsik <this0555@nate.com>
2020-10-02 12:45:49 +0900

    수정

---
+++ ex00/doc/mysql_script.sql
@@ -0,0 +1,11 @@
+use zerock_spring_20150816;
+
+create table tbl_member(
+	userid varchar(50) not null,
+	userpw varchar(50) not null,
+	username varchar(50) not null,
+	email varchar(100),
+	regdate timestamp default now(),
+	updatedate timestamp default now(),
+	primary key(userid)
+);
---
+++ ex00/src/test/java/org/zerock/web/MemberDAOTest.java
@@ -0,0 +1,52 @@
+package org.zerock.web;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.MemberVO;
+import org.zerock.persistence.MemberDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class MemberDAOTest {
+
+	@Inject
+	private MemberDAO dao;
+	
+//	@Test
+	public void testTime() throws Exception {
+		
+		System.out.println(dao.getTime());
+	}
+	
+//	@Test
+	public void testInsertMember() throws Exception {
+		
+		MemberVO vo = new MemberVO();
+		vo.setUserid("user00");
+		vo.setUserpw("user00");
+		vo.setUsername("USER00");
+		vo.setEmail("user00@aaa.com");
+		
+		dao.insertMember(vo);
+	}
+	
+//	@Test
+	public void testReadMember() throws Exception {
+		
+		MemberVO vo = (MemberVO)dao.readMember("user00");
+		
+		System.out.println(vo.toString());
+	}
+	
+	@Test
+	public void testReadWithPW() throws Exception {
+		
+		MemberVO vo = (MemberVO)dao.readWithPW("user00", "user00");
+		
+		System.out.println(vo.toString());
+	}
+}
