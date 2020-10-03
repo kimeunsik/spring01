b977f897f266acd105de2254b5fa34a52da52a35
kimeunsik <this0555@nate.com>
2020-10-03 13:02:04 +0900

    수정

---
+++ ex01/doc/mysql_script.sql
@@ -0,0 +1,11 @@
+use zerock_spring_20150816;
+
+create table tbl_board(
+	bno int not null auto_increment,
+	title varchar(200) not null,
+	content text null,
+	writer varchar(50) not null,
+	regdate timestamp not null default now(),
+	viewcnt int default 0,
+	primary key(bno)
+);
